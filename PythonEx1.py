2c5a4f1a08c8011b7860e8d0a91b04031324f531
VIDHYA <vidhyak.tech@gmail.com>
2026-07-23 12:08:33 +0530

    Basic Python Learning Exercise
    Basic Python Learning Exercise

---
+++ PythonEx1.py
@@ -0,0 +1,107 @@
+
+#1. Take two numbers as input and print their sum.
+
+a = int(input('Enter A number : '))
+b = int(input('Enter B number : '))
+c = a+b
+print('The Sum of ', a ,'and ', b ,' : ', c)
+
+#---------------------------------------------------------
+
+#2. Get two numbers and print their difference.
+
+
+a = int(input('Enter A number : '))
+b = int(input('Enter B number : '))
+c = a-b
+print('The Difference of ', a ,'and ', b ,' : ', c)
+
+#---------------------------------------------------------
+
+#3. Ask the user to enter two numbers and print their product.
+
+a = int(input('Enter A number : '))
+b = int(input('Enter B number : '))
+c = a*b
+print('The Product of ', a ,'and ', b ,' : ', c)
+
+#---------------------------------------------------------
+
+#4. Take a number and print its square.
+
+a = int(input('Enter A number : '))
+b = a*a
+print('The Square of ', a ,'is ',' : ', b)
+
+#---------------------------------------------------------
+
+#5. Take the base and height of a triangle from the user and calculate the area.
+
+a = int(input('Enter the Base of a Triangle : '))
+b = int(input('Enter the Height of a Traingle : '))
+print('Area of a Triangle = (Base*Height)/2 ')
+c = (a*b)/2
+print('The Area of a given Triangle is ', ' : ', c)
+
+#---------------------------------------------------------
+
+#  6.Ask the user for two numbers and print the quotient and remainder when the first is divided by the second.
+
+a = int(input('Enter the first number (dividend): '))
+b = int(input('Enter the second number (divisor): '))
+c = a//b
+d = a%b
+print('Results for ',a , 'divided by ', b,':')
+print('Quotient: ',c)
+print('Remainder: ',d)
+
+#---------------------------------------------------------
+
+#7.Take two numbers and print the result of raising the first number to the power of the second.
+
+a = float(input("Enter the base number: "))
+b = float(input("Enter the exponent number: "))
+c = a**b
+print(a,'raised to the power of ', b ,'is ',' : ', c)
+
+#---------------------------------------------------------
+
+#8.Ask the user to enter a total number of days and convert it into weeks and days.
+
+a = int(input('Enter the total number of days: '))
+b = a//7
+c = a%7
+print(a,' days is equal to',':',b, 'weeks and ',c, 'days.')
+#---------------------------------------------------------
+
+#9.Take three numbers and calculate their average.
+
+a = float(input('Enter A number : '))
+b = float(input('Enter B number : '))
+c = float(input('Enter C number : '))
+d = (a+b+c)/3
+print('The Average of ', a ,',', b ,'and ',c,' is : ', d)
+#---------------------------------------------------------
+
+#10. Ask the user to enter marks in 3 subjects and print the total and percentage.
+
+a = int(input('Enter the Mark 1 : '))
+b = int(input('Enter the Mark 2 : '))
+c = int(input('Enter the Mark 3  : '))
+d = a+b+c
+e = (d/300)*100
+print('Total Marks is :', d ,'out of 300.')
+print('Percentage :', e, '%')
+
+#---------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
